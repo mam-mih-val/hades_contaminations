24e870adbd63ad23edb3d5a173e4d19af9773d8c
mam-mih-val <mam.mih.val@gmail.com>
2020-12-13 17:35:16 +0300

    Ready for Ag+Ag data

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -24,16 +24,7 @@ module load compiler/gcc/9
 echo "loading " $ownroot
 source $ownroot
 
-echo "executing $build_dir/analyse -i list.txt -o AuAu@1.23AGeV_PT2.root -p 2"
-$build_dir/analyse -i list.txt -o AuAu@1.23AGeV_PT2.root -p 2
-
-echo "executing $build_dir/analyse -i list.txt -o AuAu@1.23AGeV_PT2.root -p 2 -s"
-$build_dir/analyse -i list.txt -o AuX@1.23AGeV_PT2.root -p 2 -s
-
-echo "executing $build_dir/analyse -i list.txt -o AuAu@1.23AGeV_PT3.root -p 3"
-$build_dir/analyse -i list.txt -o AuAu@1.23AGeV_PT3.root -p 3
-
-echo "executing $build_dir/analyse -i list.txt -o AuX@1.23AGeV_PT3.root -p 3 -s"
-$build_dir/analyse -i list.txt -o AuX@1.23AGeV_PT3.root -p 3 -s
+echo "executing $build_dir/analyse -i list.txt -o output.root"
+$build_dir/analyse -i list.txt -o output.root
 
 echo JOB FINISHED!
